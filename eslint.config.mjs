3aa72b16854970763b68d1665d78e950cb015398
Ben Christianson <thhuntertgm@gmail.com>
2024-03-13 12:32:11 -0500

    Adopt new eslint configs

---
+++ eslint.config.mjs
@@ -0,0 +1,42 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import globals from 'globals';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+  {
+    ignores: ['public/**'],
+  },
+  {
+    files: ['eslint.config.mjs'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+    ...tseslint.configs.disableTypeChecked,
+  },
+  {
+    files: ['webpack.config.js'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-var-requires': 'off',
+    },
+  },
+);
