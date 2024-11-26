e39e711b08d5b4c518c2a3d4432177c18cc20faf
Ben Christianson <thhuntertgm@gmail.com>
2024-11-26 13:12:18 -0600

    feat: remove mui, use tailwind

--- src/@types/SupportedLanguages.ts
+++
@@ -1,4 +0,0 @@
-const langs = ["en", "jp"] as const;
-
-export default langs;
-export type SupportedLanguage = (typeof langs)[number];
--- src/Sparky.tsx
+++ src/Sparky.tsx
@@ -1,58 +1,5 @@
-import { Container, Paper, Stack, ToggleButton, ToggleButtonGroup } from "@mui/material";
-import { Box } from "@mui/material";
 import * as React from "react";
-import { useState, useEffect } from "react";
-import { View as CalculatorView } from "./calculator/View";
-import { useTranslation } from "react-i18next";
-import { SupportedLanguage } from "./@types/SupportedLanguages";
-import Backdrop from "./images/backdrop.webp";
 
 export function Sparky() {
-  const [language, setLanguage] = useState<SupportedLanguage>(navigator.language === "ja-JP" ? "jp" : "en");
-  const { t, i18n } = useTranslation();
-
-  useEffect(() => {
-    document.title = t("page-title");
-  }, []);
-
-  useEffect(() => {
-    i18n.changeLanguage(language).catch(console.error);
-    document.documentElement.setAttribute("lang", language);
-  }, [language]);
-
-  return (
-    <Box sx={styles.bodyProxy}>
-      <Container sx={styles.container} maxWidth="xs">
-        <Stack spacing={1} alignItems="center">
-          <CalculatorView />
-          <Paper>
-            <ToggleButtonGroup
-              color="primary"
-              size="small"
-              exclusive
-              value={language}
-              onChange={(_, v: SupportedLanguage) => setLanguage(v)}
-            >
-              <ToggleButton value="en">A</ToggleButton>
-              <ToggleButton value="jp">あ</ToggleButton>
-            </ToggleButtonGroup>
-          </Paper>
-        </Stack>
-      </Container>
-    </Box>
-  );
+  return <h1>Hello, World!</h1>;
 }
-
-const styles = {
-  container: {
-    padding: 1,
-  },
-  bodyProxy: {
-    height: "100vh",
-    display: "flex",
-    alignItems: "center",
-    backgroundImage: `url(${Backdrop})`,
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "cover",
-  },
-};
