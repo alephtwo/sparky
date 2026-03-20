cd1bc5e1931d60145dafa2dfd2b3259dbac755da
Ben Christianson <thhuntertgm@gmail.com>
2026-03-20 11:01:21 -0500

    feat: add better legibility

--- src/view/components/CalculatorRow.tsx
+++ src/view/components/CalculatorRow.tsx
@@ -20,17 +20,22 @@ export const CalculatorRow: Component<CalculatorRowProps> = (props) => {
   };
 
   return (
-    <div class="flex gap-2 max-h-14">
-      <img src={props.icon} />
-      <input
-        // use "tel" to force mobile phones to use numpad, but not have the wonky html5 number api
-        type="tel"
-        autocomplete="off"
-        placeholder={props.placeholder}
-        class="input input-xl w-full text-sm font-bold"
-        value={props.value}
-        onInput={handleInput}
-      />
+    <div class="flex gap-2 h-14">
+      <img src={props.icon} class="h-full aspect-square object-cover flex-shrink-0" />
+      <div class="relative w-full">
+        <input
+          // use "tel" to force mobile phones to use numpad, but not have the wonky html5 number api
+          type="tel"
+          autocomplete="off"
+          placeholder=" "
+          class="input w-full h-full px-4 pt-2 pb-1 text-base font-bold peer"
+          value={props.value}
+          onInput={handleInput}
+        />
+        <label class="absolute left-4 top-1/2 -translate-y-1/2 text-base font-bold text-gray-400 pointer-events-none transition-all duration-200 peer-focus:top-1 peer-focus:-translate-y-0 peer-focus:text-xs peer-focus:font-semibold peer-focus:text-gray-600 peer-[:not(:placeholder-shown)]:top-1 peer-[:not(:placeholder-shown)]:-translate-y-0 peer-[:not(:placeholder-shown)]:text-xs peer-[:not(:placeholder-shown)]:font-semibold peer-[:not(:placeholder-shown)]:text-gray-600">
+          {props.placeholder}
+        </label>
+      </div>
     </div>
   );
 };
